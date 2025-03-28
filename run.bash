038e0282e36fe33907566bc922b0d6f311dd0fe7
AIAI333 <12312107@mail.sustech.edu.cn>
2025-03-28 22:59:33 +0800

    Update bash file

--- run.bash
+++ run.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python main.py --dataset camelyon17 --model transmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.5 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr
+python main.py --dataset camelyon17 --model transmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr
 
-python main.py --dataset camelyon17 --model transmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.5 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps
+python main.py --dataset camelyon17 --model transmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps
 
-python main.py --dataset camelyon17 --model attention --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.5 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr
+python main.py --dataset camelyon17 --model attention --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr
 
-python main.py --dataset camelyon17 --model attention --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.5 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps
+python main.py --dataset camelyon17 --model attention --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps
 
-python main.py --dataset camelyon17 --model rrtmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.5 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr
+python main.py --dataset camelyon17 --model rrtmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr
 
-python main.py --dataset camelyon17 --model rrtmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.5 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps
+python main.py --dataset camelyon17 --model rrtmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps
