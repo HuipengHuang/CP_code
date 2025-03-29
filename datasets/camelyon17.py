e5602fac165a5f3fdcde2f49ad41c617c196d522
AIAI333 <12312107@mail.sustech.edu.cn>
2025-03-29 14:58:33 +0800

    update dataset

--- datasets/camelyon17.py
+++ datasets/camelyon17.py
@@ -2,8 +2,42 @@ import torch
 from torch.utils.data import Dataset
 from torchvision.transforms import transforms
 from torchvision import models
+from tqdm import tqdm
 
 class MILCamelyon17(Dataset):
+    def __init__(self, dataset, device, transform=transforms.ToTensor()):
+        self.device = device
+        self.feature_extractor_part = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1).to(self.device)
+        for param in self.feature_extractor_part.parameters():
+            param.requires_grad = False
+
+        dict ={}
+        for i in range(len(dataset)):
+            img, label, metadata = dataset[i]
+            img_tensor = transform(img).to(self.device)
+            label = label.to(self.device)
+            metadata = tuple(metadata.tolist())
+            if metadata not in dict:
+                dict[metadata] = [self.feature_extractor_part(img_tensor.unsqueeze(0)), label]
+            else:
+                dict[metadata][0] = torch.cat((dict[metadata][0], self.feature_extractor_part(img_tensor.unsqueeze(0))), dim=0)
+
+        self.bag = []
+        self.label = []
+        for val in dict.values():
+            self.bag.append(val[0])
+            self.label.append(val[1])
+
+    def __len__(self):
+        return len(self.label)
+
+    def __getitem__(self, idx):
+        data = self.bag[idx]
+        label = self.label[idx]
+        return data, label
+
+
+"""class MILCamelyon17(Dataset):
     def __init__(self, dataset, device, transform=transforms.ToTensor()):
         self.device = device
         self.dataset = dataset
@@ -14,13 +48,12 @@ class MILCamelyon17(Dataset):
         for param in self.feature_extractor_part.parameters():
             param.requires_grad = False
 
-        for i in range(len(dataset)):
+        for i in tqdm(range(len(dataset))):
             _, _, metadata = dataset[i]
             metadata = tuple(metadata.tolist())
             if metadata not in self.metadata_dict:
                 self.metadata_dict[metadata] = []
             self.metadata_dict[metadata].append(i)
-        self.metadata_keys = list(self.metadata_dict.keys())
     def __len__(self):
         return len(list(self.metadata_dict.keys()))
 
@@ -38,4 +71,4 @@ class MILCamelyon17(Dataset):
 
         bag = torch.cat(bag_tensor, dim=0)
         label = label.to(self.device)
-        return bag, label
+        return bag, label"""
