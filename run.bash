20653b5193ada9325e71a6ecfc59e99bfb2270f3
AIAI333 <12312107@mail.sustech.edu.cn>
2025-03-29 20:39:19 +0800

    change run.bash

--- run.bash
+++ run.bash
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-python main.py --dataset camelyon17 --model transmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr
+for i in {1..10}; do     python main.py --dataset camelyon17 --model transmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr --seed $i --save_feature False --optimizer adam -lr 0.0001; done
 
-python main.py --dataset camelyon17 --model transmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps
+for i in {10..20}; do     python main.py --dataset camelyon17 --model transmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps --random True --seed $i --save_feature False --optimizer adam -lr 0.0001; done
 
-python main.py --dataset camelyon17 --model attention --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr
+for i in {20..30}; do     python main.py --dataset camelyon17 --model rrtmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr --seed $i --save_feature False --optimizer adam -lr 0.0001; done
 
-python main.py --dataset camelyon17 --model attention --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps
-
-python main.py --dataset camelyon17 --model rrtmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr
-
-python main.py --dataset camelyon17 --model rrtmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 2 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps
+for i in {30..40}; do     python main.py --dataset camelyon17 --model rrtmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps --random True --seed $i --save_feature False --optimizer adam -lr 0.0001; done
