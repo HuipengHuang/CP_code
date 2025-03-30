5d377b9b16a0e60fefd405f665afdb43ca3a9986
AIAI333 <12312107@mail.sustech.edu.cn>
2025-03-30 21:32:10 +0800

    u

--- run1.bash
+++ run1.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 for i in {15..20}; do
     python main.py --dataset camelyon16 --model rrtmil --save True --epoch 200 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function softmax --loss standard --test_score aps --random True --seed $i --save_feature False --optimizer adam -lr 0.0002 --weight_decay 1e-5
 done
