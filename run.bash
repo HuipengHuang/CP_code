78183796edd58d438d18b4ddb67af9868ed608b1
AIAI333 <12312107@mail.sustech.edu.cn>
2025-03-30 17:07:47 +0800

    Update run.bash

--- run.bash
+++ run.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-for i in {1..10}; do     python main.py --dataset camelyon17 --model transmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 7 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr --seed $i --save_feature False --optimizer adam -lr 0.0001; done
+for i in {1..10}; do     python main.py --dataset camelyon16 --model transmil --save True --epoch 200 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr --seed $i --save_feature False --optimizer adam -lr 0.0002 --weight_decay 1e-5; done
 
-for i in {10..20}; do     python main.py --dataset camelyon17 --model transmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 7 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps --random True --seed $i --save_feature False --optimizer adam -lr 0.0001; done
+for i in {10..20}; do     python main.py --dataset camelyon16 --model transmil --save True --epoch 200 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps --random True --seed $i --save_feature False --optimizer adam -lr 0.0002 --weight_decay 1e-5; done
 
-for i in {20..30}; do     python main.py --dataset camelyon17 --model rrtmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 7 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr --seed $i --save_feature False --optimizer adam -lr 0.0001; done
+for i in {20..30}; do     python main.py --dataset camelyon16 --model rrtmil --save True --epoch 200 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr --seed $i --save_feature False --optimizer adam -lr 0.0002 --weight_decay 1e-5; done
 
-for i in {30..40}; do     python main.py --dataset camelyon17 --model rrtmil --save True --epoch 100 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 7 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps --random True --seed $i --save_feature False --optimizer adam -lr 0.0001; done
+for i in {30..40}; do     python main.py --dataset camelyon16 --model rrtmil --save True --epoch 200 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps --random True --seed $i --save_feature False --optimizer adam -lr 0.0002 --weight_decay 1e-5; done
+
+for i in {40..50}; do     python main.py --dataset camelyon16 --model gradattention --save True --epoch 200 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score thr --seed $i --save_feature False --optimizer adam -lr 0.0002 --weight_decay 1e-5; done
+
+for i in {50..60}; do     python main.py --dataset camelyon16 --model gradattention --save True --epoch 200 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function sigmoid --loss bce --test_score aps --random True --seed $i --save_feature False --optimizer adam -lr 0.0002 --weight_decay 1e-5; done
