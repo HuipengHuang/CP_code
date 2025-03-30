3583366c804d1a89598c6da16f002267693cc8af
AIAI333 <12312107@mail.sustech.edu.cn>
2025-03-30 18:58:32 +0800

    Update bce loss

--- loss/bce_loss.py
+++ loss/bce_loss.py
@@ -1,11 +1,10 @@
 from .base_loss import BaseLoss
 import torch
+from torch.nn import BCEWithLogitsLoss
 class BinaryCrossEntropyLoss(BaseLoss):
     def __init__(self):
         super().__init__()
 
     def forward(self, logit, target):
-        prob = torch.sigmoid(logit)
-        target = target.float()
-        loss = torch.mean(-1. * (target * torch.log(prob[:,1] + 1e-8) + (1. - target) * torch.log(1. - prob[:, 1] + 1e-8)))
+        loss = BCEWithLogitsLoss()(logit, target)
         return loss
