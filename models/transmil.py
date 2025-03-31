741a8183a1f1643600cab25ae073f03a8ea83022
AIAI333 <12312107@mail.sustech.edu.cn>
2025-03-31 10:55:18 +0800

    Update transmil

--- models/transmil.py
+++ models/transmil.py
@@ -8,9 +8,9 @@ from .embed_position import PPEG
 
 class TransLayer(nn.Module):
 
-    def __init__(self, norm_layer=nn.LayerNorm, dim=512):
+    def __init__(self, device, norm_layer=nn.LayerNorm, dim=512):
         super().__init__()
-        self.norm = norm_layer(dim)
+        self.norm = norm_layer(dim).to(device)
         self.attn = NystromAttention(
             dim=dim,
             dim_head=dim // 8,
@@ -21,7 +21,7 @@ class TransLayer(nn.Module):
             residual=True,
             # whether to do an extra residual with the value or not. supposedly faster convergence if turned on
             dropout=0.1
-        )
+        ).to(device)
 
     def forward(self, x):
         x = x + self.attn(self.norm(x))
@@ -33,20 +33,27 @@ class TransMIL(nn.Module):
     def __init__(self, device, n_classes=2):
         super(TransMIL, self).__init__()
 
-        self.pos_layer = PPEG(dim=512)
-        self._fc1 = nn.Sequential(nn.Linear(1024, 512), nn.ReLU())
+        self.pos_layer = PPEG(dim=512).to(device)
+        self._fc1 = nn.Sequential(nn.Linear(1024, 512), nn.ReLU()).to(device)
         self.cls_token = nn.Parameter(torch.randn(1, 1, 512)).to(device)
         self.n_classes = n_classes
-        self.layer1 = TransLayer(dim=512)
-        self.layer2 = TransLayer(dim=512)
-        self.norm = nn.LayerNorm(512)
-        self._fc2 = nn.Linear(512, self.n_classes)
+        self.layer1 = TransLayer(dim=512, device=device)
+        self.layer2 = TransLayer(dim=512, device=device)
+        self.norm = nn.LayerNorm(512).to(device)
+        self._fc2 = nn.Linear(512, self.n_classes).to(device)
         self.device = device
 
     def forward(self, h):
+        h = h.to(self.device)
         h = self._fc1(h)  # [B, n, 512]
         # ---->cls_token
         B = h.shape[0]
+        H = h.shape[1]
+
+        _H, _W = int(np.ceil(np.sqrt(H))), int(np.ceil(np.sqrt(H)))
+        add_length = _H * _W - H
+        h = torch.cat([h, h[:, :add_length, :]], dim=1)
+
         cls_tokens = self.cls_token.expand(B, -1, -1)
         h = torch.cat((cls_tokens, h), dim=1)
 
