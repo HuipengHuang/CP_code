5fdd4e698ee1e9e26f725f3b767bbd173152bd17
AIAI333 <12312107@mail.sustech.edu.cn>
2025-03-31 21:39:08 +0800

    bug

--- data_preprocess/csv2pth.py
+++ data_preprocess/csv2pth.py
@@ -13,9 +13,9 @@ def csv2pth(data_path, save_path):
         os.makedirs(save_path + "/test/data", exist_ok=True)
         os.makedirs(save_path + "/train/data", exist_ok=True)
         with open(save_path + "/train/label.csv", 'w') as f:
-            pass
+            f.write("")
         with open(save_path + "/test/label.csv", 'w') as f:
-            pass
+            f.write("")
 
     train_writer = csv.writer(open(save_path + "/train/label.csv", 'w', newline=""),)
     train_writer.writerow(['filename', 'label'])
