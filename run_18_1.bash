6e02fb570dc7ba98cd7f90e65e1289103e974ac7
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-01 21:55:19 +0800

    update tcga experiment

--- run_18_1.bash
+++ run_18_1.bash
@@ -13,4 +13,3 @@ done
 for i in {10..15}; do
     python main.py --dataset camelyon16 --model rrtmil --save True --epoch 200 --cal_ratio 0.5 --batch_size 1 --alpha 0.1 --gpu 0 -mil True -auc True --final_activation_function softmax --loss standard --test_score thr --seed $i --save_feature False --optimizer adam -lr 0.0002 --weight_decay 1e-5 --extract_feature_model resnet18 --input_dimension 512 --save_memory True
 done
-
