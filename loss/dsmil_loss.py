113c99a0ec4fd872f159a5fbe2baa60fe5bf14ec
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-03 20:15:45 +0800

    update

--- loss/dsmil_loss.py
+++ loss/dsmil_loss.py
@@ -16,6 +16,8 @@ class DSMilLoss(BaseLoss):
     def forward(self, input_list, target):
         ins_prediction, bag_prediction, _, _ = input_list
         max_prediction, _ = torch.max(ins_prediction, 0)
+        print(bag_prediction.shape)
+        print(max_prediction.shape)
         bag_loss = self.criterion(bag_prediction.view(1, -1), target.view(1, -1))
         max_loss = self.criterion(max_prediction.view(1, -1), target.view(1, -1))
         loss = 0.5 * bag_loss + 0.5 * max_loss
