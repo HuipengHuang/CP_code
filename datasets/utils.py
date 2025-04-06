b20c831888bf61cf8c5e237dc06a45af97d50e31
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-06 22:50:54 +0800

    xx

--- datasets/utils.py
+++ datasets/utils.py
@@ -66,7 +66,7 @@ def build_dataset(args):
             train_size = len(mil_dataset) - test_size - cal_size
             mil_train_dataset, mil_cal_dataset, mil_test_dataset = random_split(mil_dataset, [train_size, cal_size, test_size])
 
-        elif args.extract_feature_model == "resnet50":
+        elif args.extract_feature_model == "resnet501":
             save_path = "./data/camelyon16_rn50_feature_new_new"
             mil_train_dataset = MILCamelyon16_rn50(device=device, path=save_path, train=True)
             mil_cal_test_dataset = MILCamelyon16_rn50(device=device, path=save_path, train=False)
@@ -74,7 +74,7 @@ def build_dataset(args):
             cal_size = int(args.cal_ratio * len(mil_cal_test_dataset))
             test_size = len(mil_cal_test_dataset) - cal_size
             mil_cal_dataset, mil_test_dataset = random_split(mil_cal_test_dataset, [cal_size, test_size])
-        elif args.extract_feature_model == "test":
+        elif args.extract_feature_model == "resnet50":
             save_path = "./data/camelyon16_rn50_feature_new_new"
             mil_train_dataset = MILCamelyon16_rn50(device=device, path=save_path, train=True)
             mil_cal_test_dataset = MILCamelyon16_rn50(device=device, path=save_path, train=False)
