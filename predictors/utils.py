9f9edd9aca054d2bc56ca15c251141e0d017b18c
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-10 17:05:04 +0800

    update

--- predictors/utils.py
+++ predictors/utils.py
@@ -5,7 +5,7 @@ import torch
 def get_predictor(args, net, num_classes, adapter=None, final_activation_function="softmax"):
     if args.model == "dsmil":
         predictor = MilPredictor(args, net, num_classes, final_activation_function, adapter)
-    elif args.arrgregation is not None:
+    elif args.aggregation is not None:
         if args.aggregation == "max":
             predictor = MaxPredictor(args, net, num_classes, final_activation_function, adapter)
         else:
