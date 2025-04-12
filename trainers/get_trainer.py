12e893daa0eb7f07dba14fa6844a5cbda068e91b
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-12 20:20:56 +0800

    update

--- trainers/get_trainer.py
+++ trainers/get_trainer.py
@@ -4,7 +4,7 @@ from .dtfd_trainer import DFDT_Trainer
 def get_trainer(args, num_classes):
     if args.algorithm == "uatr":
         trainer = UncertaintyAwareTrainer(args, num_classes)
-    elif args.model == "dtfd":
+    elif args.model == "dtfdmil":
         trainer = DFDT_Trainer(args, num_classes)
     else:
         trainer = Trainer(args, num_classes)
