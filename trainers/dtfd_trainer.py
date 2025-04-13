45778d3ec832af78e92ebcb071eff600f5ee75da
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-13 14:25:09 +0800

    update

--- trainers/dtfd_trainer.py
+++ trainers/dtfd_trainer.py
@@ -249,7 +249,6 @@ class DFDT_Trainer:
                 self.train_loop(train_loader, epoch)
         else:
             for epoch in range(epochs):
-                accuracy, auc_value, precision, recall, fscore, loss = self.val_loop(val_loader)
                 self.train_loop(train_loader, epoch)
                 accuracy, auc_value, precision, recall, fscore, loss = self.val_loop(val_loader)
                 stop = self.early_stopping(loss, epoch)
