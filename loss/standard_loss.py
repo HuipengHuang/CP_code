04cd0c5d4f5b645bd1c2a50a8e4961d13ec557a6
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-19 23:09:06 +0800

    fix bug

--- loss/standard_loss.py
+++ loss/standard_loss.py
@@ -10,7 +10,6 @@ class StandardLoss(BaseLoss):
         else:
             self.T = 1
     def forward(self, logits, target):
-        print(logits.shape)
-        print(target.shape)
+        target = target.view(-1)
         loss = nn.CrossEntropyLoss()(logits / self.T, target)
         return loss
