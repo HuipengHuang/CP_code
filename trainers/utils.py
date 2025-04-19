66258fb543c1606c87fcc724ec949c6ba650c270
AIAI333 <12312107@mail.sustech.edu.cn>
2025-04-19 23:14:07 +0800

    fix bug

--- trainers/utils.py
+++ trainers/utils.py
@@ -12,7 +12,10 @@ def five_scores(bag_labels, bag_predictions,sub_typing=False):
     fpr, tpr, threshold = roc_curve(bag_labels, bag_predictions, pos_label=1)
     fpr_optimal, tpr_optimal, threshold_optimal = optimal_thresh(fpr, tpr, threshold)
     # threshold_optimal=0.5
-    auc_value = roc_auc_score(bag_labels, bag_predictions)
+    if sub_typing:
+        auc_value = roc_auc_score(bag_labels, bag_predictions, multi_class='ovr')
+    else:
+        auc_value = accuracy_score(bag_labels, bag_predictions)
     this_class_label = np.array(bag_predictions)
     this_class_label[this_class_label>=threshold_optimal] = 1
     this_class_label[this_class_label<threshold_optimal] = 0
